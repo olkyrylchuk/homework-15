dab1ff910b45cfb9b40eddc15268594701848e3b
Aleksandr Kyrylchuk <al.kyrylchuk@gmail.com>
2022-01-29 17:11:52 +0100

    first commit

---
+++ min/js/javascript.js
@@ -0,0 +1,11 @@
+let a = 0.1;
+let b = 0.2;
+alert(`сумма чисел 0.1 и 0.2 равна ${a + b}`);
+
+// let string = Number('1');
+let string = ('1');
+let num = 2;
+alert(`сумма чисел 1 и 2 равна ${(+string + num)}`);
+
+let flashCard = prompt('укажиет размер флешки в GB');
+alert(`на карту памяти возможно записать ${flashCard *1000 / 820} файлов`);
